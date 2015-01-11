6392fe325be02be27b836ba2e6a57ee2d5debab9
Yanzheng Li <yli@hushmail.me>
2015-01-11 13:12:02 -0800

    include format

--- src/frontend/utils.cc
+++ src/frontend/utils.cc
@@ -21,6 +21,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *******************************************************************************/
 #include "../../include/frontend/utils.h"
+
 #include "../../include/runtime/instr_block.h"
 #include "../../include/runtime/instr.h"
 
