539bc314f33eeac37620389dc0364bc6669f94f1
Yanzheng Li <yli@hushmail.me>
2015-01-11 19:11:22 -0800

    include guard

--- include/frontend/bytecode_loader.h
+++ include/frontend/bytecode_loader.h
@@ -20,8 +20,8 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 *******************************************************************************/
-#ifndef COREVM_FRONTEND_BYTECODE_LOADER_H_
-#define COREVM_FRONTEND_BYTECODE_LOADER_H_
+#ifndef COREVM_BYTECODE_LOADER_H_
+#define COREVM_BYTECODE_LOADER_H_
 
 #include "errors.h"
 #include "../runtime/process.h"
@@ -49,4 +49,4 @@ void load(const std::string&, corevm::runtime::process&) throw(corevm::frontend:
 } /* end namespace corevm */
 
 
-#endif /* COREVM_FRONTEND_BYTECODE_LOADER_H_ */
+#endif /* COREVM_BYTECODE_LOADER_H_ */
