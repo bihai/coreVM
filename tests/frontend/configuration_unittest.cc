cdee08863df2b5717c037358cb046b75873f5b73
Yanzheng Li <yli@hushmail.me>
2015-02-17 21:49:36 -0800

    unit test

---
+++ tests/frontend/configuration_unittest.cc
@@ -0,0 +1,101 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2015 Yanzheng Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*******************************************************************************/
+#include "../../include/frontend/configuration.h"
+
+#include <sneaker/json/json.h>
+#include <sneaker/testing/_unittest.h>
+
+#include <fstream>
+#include <string>
+
+
+class configuration_unittest : public ::testing::Test
+{
+protected:
+  static const char* PATH;
+
+  virtual void SetUp()
+  {
+    std::ofstream f(PATH, std::ios::binary);
+    f << this->content();
+    f.close();
+  }
+
+  virtual void TearDown()
+  {
+    remove(PATH);
+  }
+
+  virtual const std::string content()
+  {
+    static const std::string content(
+      "{"
+        "\"alloc-size\": 1024"
+      "}"
+    );
+
+    return content;
+  }
+};
+
+// -----------------------------------------------------------------------------
+
+const char* configuration_unittest::PATH = "./sample-config.config";
+
+// -----------------------------------------------------------------------------
+
+TEST_F(configuration_unittest, TestLoadSuccessful)
+{
+  JSON config_json;
+
+  corevm::frontend::configuration configuration(PATH);
+
+  configuration.load_config(&config_json);
+
+  ASSERT_EQ(JSON::Type::OBJECT, config_json.type());
+
+  JSON::object config_obj = config_json.object_items();
+
+  JSON alloc_size_raw = config_obj.at("alloc-size");
+  int alloc_size = static_cast<int>(alloc_size_raw.int_value());
+
+  ASSERT_EQ(1024, alloc_size);
+}
+
+// -----------------------------------------------------------------------------
+
+TEST_F(configuration_unittest, TestLoadFailsWithInvalidPath)
+{
+  JSON config_json;
+
+  corevm::frontend::configuration configuration("$%^some-invalid-path!@#");
+
+  ASSERT_THROW(
+    {
+      configuration.load_config(&config_json);
+    },
+    corevm::frontend::configuration_loading_error
+  );
+}
+
+// -----------------------------------------------------------------------------
