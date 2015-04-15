c92b16788ff38955f0d210e8c9883dade90c90af
Yanzheng Li <yli@hushmail.me>
2015-04-14 22:33:41 -0700

    use iteration protocol internally in sources

--- python/src/tuple.py
+++ python/src/tuple.py
@@ -46,12 +46,21 @@ class tuple(object):
         index = __call(int, 0)
         res = __call(str, '')
         __call(res.__add__, __call(str, '('))
-        for item in self:
-            __call(res.__add__, __call(item.__repr__)) # res += str(item)
-            if __call(index.__lt__, top_index):
-                __call(res.__add__, __call(str, ', '))
-            index = __call(index.__add__, 1)
-        __call(res.__add__, __call(str, ')'))
+
+        iterator_ = __call(self.__iter__)
+        try:
+            while True:
+                item = __call(iterator_.next)
+
+                __call(res.__add__, __call(item.__repr__)) # res += str(item)
+
+                if __call(index.__lt__, top_index):
+                    __call(res.__add__, __call(str, ', '))
+
+                __call(index.__iadd__, __call(int, 1))
+        except StopIteration:
+            __call(res.__add__, __call(str, ')'))
+
         return res
 
     def __repr__(self):
